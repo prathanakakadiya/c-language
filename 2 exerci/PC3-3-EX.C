196430666df7e258e55d6f6c435fee09d8fbfd75
prathanakakadiya <132265061+prathanakakadiya@users.noreply.github.com>
2023-05-16 04:28:59 -0700

    Add files via upload

---
+++ 2 exerci/PC3-3-EX.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+int b;
+int c;
+clrscr();
+printf("enter value= ");
+scanf("%d",&a);
+printf("enter value= ");
+scanf("%d",&b);
+printf("answer=%d",180-(a+b));
+getch();
+}
