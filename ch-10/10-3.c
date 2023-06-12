978d298b6005047833eb5f66bfd0dc77a8bfebd1
prathanakakadiya <132265061+prathanakakadiya@users.noreply.github.com>
2023-06-12 04:13:53 -0700

    Add files via upload

--- ch-10/10-3.c
+++ ch-10/10-3.c
@@ -2,14 +2,17 @@
 
 void sum()
 {
-	int a;
+	int a,b,avg,n;
 	printf("enter a : ");
 	scanf("%d",&a);
-	
-	
-	printf("cube :%d",a*a*a);
+	printf("enter b : ");
+	scanf("%d",&b);
+	n=
+	avg=a+b/2;
+	printf("cube :%d",avg);
 }
 void main()
 {
 	sum();
 }
+
